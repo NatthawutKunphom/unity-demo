06e877e8732287d7786acfc007d1630b8c82fae9
NatthawutKunphom <108015365+NatthawutKunphom@users.noreply.github.com>
2022-07-05 14:55:11 +0700

    add code

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GamePlay : MonoBehaviour
+
 {
+    int RedSphere = -20;
+    int BuleSphere = 10;
+    int GreenSphere = 20;
+    int sum = 0;
+    public TextMeshProUGUI txtDisplay;
+
     // Update is called once per frame
     void Update () 
     {
@@ -17,8 +26,61 @@ public class GamePlay : MonoBehaviour
                 if (hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
                 {
                     hit.collider.gameObject.SetActive(false);
+                    sum = sum + RedSphere;
+                    
                 }
+                else if (hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + GreenSphere;
+                    
+                }
+                else if (hit.collider.gameObject.name.CompareTo("BuleSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + BuleSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("BuleSphere1") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + BuleSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("BuleSphere2") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + BuleSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("RedSphere1") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + RedSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("RedSphere2") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + RedSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("GreenSphere1") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + GreenSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("GreenSphere2") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + GreenSphere;
+                }
+                else if (hit.collider.gameObject.name.CompareTo("GreenSphere3") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + GreenSphere;
+                }
+
+                Debug.Log(sum);
+                txtDisplay.text =" "+sum;
+
             }
+
         }
        
     }
