1f04c0cbeaccbbd95c7c8c5f3cea41ad7f7e1d6c
NatthawutKunphom <108015365+NatthawutKunphom@users.noreply.github.com>
2022-07-05 10:49:09 +0700

    print Hello World

---
+++ Assets/myScript1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        Debug.Log(z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
